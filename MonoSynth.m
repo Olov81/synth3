e4c9ddd4bd5d76bd96e052092661a81d96b3c8dc
Olle <devnull@localhost>
2014-01-06 15:20:16 +0100

    Updated mono synth with the new oscillators.

--- MonoSynth.m
+++ MonoSynth.m
@@ -6,16 +6,27 @@ classdef MonoSynth < EmptyModule
         cvInput
         output
         
+        cutoffInput
+        resonanceInput
+        
         fenv
         fenvAmount
+        
+        aenv
+        
+        vco1Tune
     end
     
     properties(Access=private)
         
-        vco
+        vco1
+        vco2
         vcf
         vca
         
+        vcoSum
+        cutoffSum
+        vco1TuneGain
     end
     
     methods
@@ -26,26 +37,58 @@ classdef MonoSynth < EmptyModule
             
             this.gateInput = this.createInputPort();
             this.cvInput = this.createInputPort();
-           
-            this.vco = this.addSubModule( SawtoothOscillator('VCO', fs) );
+            
+            this.cutoffInput = this.createInputPort();
+            this.resonanceInput = this.createInputPort();
+            
+            this.vco1Tune = this.createInputPort();
+            
+            this.vco1 = this.addSubModule( AliasFreeSawtoothOscillator('VCO 1') );
+            this.vco2 = this.addSubModule( AliasFreeSawtoothOscillator('VCO 2') );
             this.vcf = this.addSubModule( MoogFilter('VCF', 4) );
             this.vca = this.addSubModule( Gain('VCA', 1) );
             this.fenv = this.addSubModule( EnvelopeGenerator('VCF Env', fs) );
-            this.fenvAmount = this.addSubModule( Gain('VCF Env Amout', 1) );
+            this.aenv = this.addSubModule( EnvelopeGenerator('VCA Env', fs) );
+            this.fenvAmount = this.addSubModule( Gain('VCF Env Amount', 1) );
+            this.cutoffSum = this.addSubModule( Sum('Cutoff control',2) );
+            this.vcoSum = this.addSubModule( Sum('VCO mix',2) );
+            this.vco1TuneGain = this.addSubModule( Gain('VCO 1 Tune', 1) );
             
             % Connect audio signal chain
-            this.vco.frequencyInput.connect( this.cvInput );
-            this.vcf.input.connect( this.vco.output );
-            this.fenvAmount.input.connect(  this.fenv.output );
-            this.vcf.cutoffFrequencyInput.connect( this.fenvAmount.output );
+            this.vco1TuneGain.input.connect( this.cvInput );
+            this.vco1.frequencyInput.connect( this.vco1TuneGain.output );
+            this.vco1TuneGain.gainInput.connect( this.vco1Tune );
+            this.vco2.frequencyInput.connect( this.cvInput );
+            this.vco2.phaseshift = 0.5;
+            this.vcoSum.inputPorts(1).connect( this.vco1.output );
+            this.vcoSum.inputPorts(2).connect( this.vco2.output );
+            
+            this.vcf.input.connect( this.vcoSum.output );
             this.vca.input.connect( this.vcf.output );
             
-            % Connect modulation signals
+            % Filter modulation chain
             this.fenv.gateInput.connect( this.gateInput );
-            this.fenv.attackInput.set( 1e-2 );
+            this.fenvAmount.input.connect( this.fenv.output );
+            this.cutoffSum.inputPorts(1).connect( this.fenvAmount.output );
+            this.cutoffSum.inputPorts(2).connect( this.cutoffInput );
+            
+            this.vcf.cutoffFrequencyInput.connect( this.cutoffSum.output );
+            this.vcf.resonanceInput.connect( this.resonanceInput );
+            
+            % Amplitude modulation chain
+            this.vca.gainInput.connect( this.aenv.output );
+            this.aenv.gateInput.connect( this.gateInput );
+            
+            % Default values
+            this.fenv.attackInput.set( 1e-3 );
             this.fenv.decayInput.set( 0.5 );
             this.fenv.sustainInput.set( 0.1 );
             this.fenv.releaseInput.set( 0.2 );
+            
+            this.aenv.attackInput.set( 1e-3 );
+            this.aenv.decayInput.set( 0.5 );
+            this.aenv.sustainInput.set( 0.0 );
+            this.aenv.releaseInput.set( 0.2 );
             
             this.output = this.vca.output;
         end
--- TestMonoSynth.m
+++ TestMonoSynth.m
@@ -3,24 +3,34 @@ clc;
 close all;
 
 fs = 44100;
-T = 3;
+T = 8;
 t = (0:T*fs-1)/fs;
 N = length(t);
 
 seq = MatrixSequencer('Sequencer', fs);
-seq.bpm = 100;
+seq.bpm = 120;
+seq.transpose = 5;
 
 synth = MonoSynth('Synth', fs);
 
 synth.gateInput.connect( seq.gateOutput );
 synth.cvInput.connect( seq.cvOutput );
 
-synth.fenv.decayInput.set( 0.01 );
+synth.vco1Tune.set( 0.997 );
+synth.fenv.decayInput.set( 0.03 );
 synth.fenv.sustainInput.set( 0 );
+synth.fenv.attackInput.set( 1e-5 );
+synth.fenv.releaseInput.set( 0.05 );
+
+synth.aenv.decayInput.set( 0.02 );
+synth.aenv.releaseInput.set( 0.05 );
+
 synth.fenvAmount.gainInput.set( 0.2 );
+synth.resonanceInput.set( 0.2 );
+synth.cutoffInput.set( 0.001 );
 
 synth.update(N);
 
 % plot( synth.output.read(N) );
 
-sound( 0.2*synth.output.read(N), fs );
+sound( 0.5*synth.output.read(N), fs );
