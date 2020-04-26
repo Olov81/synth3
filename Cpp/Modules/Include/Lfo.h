4c8174a289ead26d8e2ee38e1a82c7adb3ce533c
Olle <devnull@localhost>
2020-04-26 10:24:41 +0200

    Added delay to LFO

--- Cpp/Modules/Include/Lfo.h
+++ Cpp/Modules/Include/Lfo.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Source.h"
+#include "FlankDetector.h"
 
 class Lfo : public Source
 {
@@ -9,17 +10,28 @@ public:
 
 	void Update() override;
 
-	IInputPort* GetFrequencyInput() const;
+	IInputPort* FrequencyInput() const;
 
-	IInputPort* GetAmplitudeInput() const;
+	IInputPort* AmplitudeInput() const;
 
-	IInputPort* GetOffsetInput() const;
+	IInputPort* OffsetInput() const;
+
+	IInputPort* DelayInput() const;
+
+	IInputPort* GateInput() const;
 
 private:
 
 	IInputPort* _pFrequencyInput;
 	IInputPort* _pAmplitudeInput;
 	IInputPort* _pOffsetInput;
+	IInputPort* _pDelayInput;
+	IInputPort* _pGateInput;
+
 	const double _ts;
 	double _f;
+	double _timeSinceReset;
+	FlankDetector _flankDetector;
+
+	void ResetTimer();
 };
--- Cpp/Modules/Source/Lfo.cpp
+++ Cpp/Modules/Source/Lfo.cpp
@@ -5,15 +5,29 @@
 Lfo::Lfo(const double fs)
 	:_ts(1/fs)
 	,_f(0.0)
+	,_timeSinceReset(0)
+	,_flankDetector(1e-3, std::bind(&Lfo::ResetTimer, this), []{})
 {
 	_pFrequencyInput = CreateInputPort();
 	_pAmplitudeInput = CreateInputPort();
 	_pOffsetInput = CreateInputPort();
+	_pDelayInput = CreateInputPort();
+	_pGateInput = CreateInputPort();
 }
 
 void Lfo::Update()
 {
 	_f = _f + _pFrequencyInput->Read();
+	_timeSinceReset += _ts;
+
+	_flankDetector.Update(_pGateInput->Read());
+
+	if(_timeSinceReset < _pDelayInput->Read())
+	{
+		Write(0);
+		return;
+	}
+
 	const auto a = _pAmplitudeInput->Read();
 	const auto offset = _pOffsetInput->Read();
 
@@ -22,17 +36,32 @@ void Lfo::Update()
 	Write(y);
 }
 
-IInputPort* Lfo::GetFrequencyInput() const
+IInputPort* Lfo::FrequencyInput() const
 {
 	return _pFrequencyInput;
 }
 
-IInputPort* Lfo::GetAmplitudeInput() const
+IInputPort* Lfo::AmplitudeInput() const
 {
 	return _pAmplitudeInput;
 }
 
-IInputPort* Lfo::GetOffsetInput() const
+IInputPort* Lfo::OffsetInput() const
 {
 	return _pOffsetInput;
+}
+
+IInputPort* Lfo::DelayInput() const
+{	
+	return _pDelayInput;
+}
+
+IInputPort* Lfo::GateInput() const
+{
+	return _pGateInput;
+}
+
+void Lfo::ResetTimer()
+{
+	_timeSinceReset = 0;
 }
